f9afdfa7523f0df6f0791e33645c2ae83e6445a3
maxuss777 <kul-astra@ya.ru>
2014-10-01 20:20:55 +0400

    added FeedsServices

---
+++ MyZoo.Business.Services/FeedsServices.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using MyZoo.Common.Factories;
+
+namespace MyZoo.Business.Services
+{
+    public class FeedsServices : FeedFactory
+    {
+        public override void CreateFeed(int type, int number)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
