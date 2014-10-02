8b8bff9c762a48018c6c93fa23489d36034dcbab
maxuss777 <kul-astra@ya.ru>
2014-10-02 17:26:48 +0300

    new commit

---
+++ MyZoo.Business.Services.Tests/FeedsServicesTests.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+using MyZoo.Common.Feeds;
+using MyZoo.DataAccess.Core;
+using NUnit.Framework;
+using ServiceStack;
+
+namespace MyZoo.Business.Services.Tests
+{
+    public class FeedsServicesTests
+    {
+        private readonly FeedsRepository _feedsRepository = new FeedsRepository();
+        private readonly FeedsServices _feedsServices = new FeedsServices();
+
+        #region Create feeds
+
+        [Test]
+        public void CreateCageForMammal()
+        {
+            //arange
+            var FeedToCreate = new List<Feeds> { Feeds.ForMammal };
+            const Feeds actualFeed = Feeds.ForMammal;
+
+            //act
+            _feedsServices.CreateFeed(FeedToCreate);
+            var expectedFeed = _feedsRepository.GetLastCreatedFeed();
+
+            //assert
+            Assert.AreEqual(expected: expectedFeed.ToJson(), actual: actualFeed.ToJson());
+        }
+
+        [Test]
+        public void CreateFeedForBird()
+        {
+            //arange
+            var FeedToCreate = new List<Feeds> { Feeds.ForBird };
+            const Feeds expectedFeed = Feeds.ForBird;
+
+            //act
+            _feedsServices.CreateFeed(FeedToCreate);
+            var actualFeed = _feedsRepository.GetLastCreatedFeed();
+
+            //assert
+            Assert.AreEqual(expected: expectedFeed.ToJson(), actual: actualFeed.ToJson());
+        }
+
+        [Test]
+        public void CreateFeedForReptile()
+        {
+            //arange
+            var feedToCreate = new List<Feeds> { Feeds.ForReptile };
+            const Feeds actualFeed = Feeds.ForReptile;
+
+            //act
+            _feedsServices.CreateFeed(feedToCreate);
+            var expectedFeed = _feedsRepository.GetLastCreatedFeed();
+
+            //assert
+            Assert.AreEqual(expected: expectedFeed.ToJson(), actual: actualFeed.ToJson());
+        }
+
+        #endregion
+
+        #region Get all existing feeds
+
+        [Test]
+
+        public void GetAllFeedsAsList()
+        {
+            //arrange
+
+            //act
+            var expectedFeedsList = _feedsRepository.GetAll();
+            var actualFeedsList = _feedsServices.GetAllExistinfFeeds();
+
+            //assert
+            Assert.AreEqual(expected: expectedFeedsList.ToJson(), actual: actualFeedsList.ToJson());
+        }
+
+        #endregion
+    }
+}
---
+++ MyZoo.DataAccess.Core/FeedsRepository.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
+using MyZoo.Common.Feeds;
+
+
+namespace MyZoo.DataAccess.Core
+{
+    public class FeedsRepository : Repository
+    {
+        public void Insert(Feeds feed)
+        {
+            const string sql =
+               "INSERT INTO Feeds(ForWhich) Values(@ForWhich)";
+
+            using (var connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+
+                using (var command = new SqlCommand(sql, connection))
+                {
+                    command.Parameters.AddWithValue("@ForWhich", feed.ToString());
+
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public List<Feeds> GetAll()
+        {
+            const string sql = "SELECT * FROM Feeds";
+
+            var feedsList = new List<Feeds>();
+
+            using (var connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+
+                using (var command = new SqlCommand(sql, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            switch (reader["ForWhich"].ToString())
+                            {
+                                case "ForMammals":
+                                    feedsList.Add(Feeds.ForMammal);
+                                    break;
+                                case "ForBird":
+                                    feedsList.Add(Feeds.ForBird);
+                                    break;
+                                case "ForReptile":
+                                    feedsList.Add(Feeds.ForReptile);
+                                    break;
+                            }
+                        }
+                    }
+                }
+
+            }
+            return feedsList;
+        }
+
+        public Feeds GetLastCreatedFeed()
+        {
+            const string getEntities = "SELECT TOP 1 * FROM Feeds ORDER BY id DESC";
+
+
+            using (var connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+
+                using (var command = new SqlCommand(getEntities, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            switch (reader["ForWhich"].ToString())
+                            {
+                                case "ForMammal":
+                                    return Feeds.ForMammal;
+
+                                case "ForBird":
+                                    return Feeds.ForBird;
+
+                                case "ForReptile":
+                                    return Feeds.ForReptile;
+                            }
+                        }
+                    }
+                }
+            }
+            return Feeds.NoOne;
+        }
+        
+    }
+}
