ccf7b293a4e07d154464422717aaa8ae25ef3da6
maxuss777 <kul-astra@ya.ru>
2014-11-01 09:58:49 +0300

    home trying

--- MyZoo.Common.Factories/CageFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace MyZoo.Common.Factories
-{
-    public abstract class CageFactory
-    {
-        public abstract void CreateCages(List<Cages.Cages> cages);
-    }
-}
--- MyZoo.Common.Factories/FeedFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace MyZoo.Common.Factories
-{
-    public abstract class FeedFactory
-    {
-        public abstract void CreateFeeds(List<Feeds.Feeds> feeds);
-    }
-}
